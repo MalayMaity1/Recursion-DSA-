f4ffeccfdbda510bdf538263d44ccf1221dd47fd
Malay Maity <maitymalay27747@gmail.com>
2024-11-26 11:39:03 +0530

    Print the power of a number

---
+++ Print_power.java
@@ -0,0 +1,12 @@
+public class Print_power{
+
+    public static int power(int x,int y){
+        if(y==0){
+            return 1;
+        }
+        return ( x * ( power ( x , y-1 )));
+    }
+    public static void main(String args[]){
+        System.out.println(power(2, 3));
+    }
+}
